a9c867f993d49e9001f7af31acd1700bff63d805
rahmanwaliur <waliur.rahman@ucalgary.ca>
2014-04-15 11:34:15 -0600

    updated jobid retrieval logic from subprocess call result.

--- APIConsumer/getcodes.py
+++ APIConsumer/getcodes.py
@@ -5,6 +5,7 @@ import genhive
 import submitjob
 import subprocess
 import thread
+import string
 import time
 # response = requests.get('https://github.com/timeline.json')
 
@@ -13,11 +14,11 @@ with open("data.json") as filename:
   length = len(parsed_data)
   #print parsed_data
 
-firsttime = 0
+
+
 rownum = 0
 flag = 1
-i = 80
-j = 0
+
 for row in parsed_data:
     if (length == rownum+1):
         flag = 0
@@ -28,13 +29,23 @@ for row in parsed_data:
     hivequery = genhive.hivequery(user, cohort, jobparams)
 
     thread.start_new_thread(submitjob.newjob,(hivequery,flag))
-    time.sleep(7)
+
+    #time.sleep(7)
     #alljobs = subprocess.check_output('/usr/local/hadoop/bin/hadoop job -list', shell=True)
-    proc = subprocess.Popen(["/usr/lib/hadoop/bin/hadoop", "job", "-list"], stdout=subprocess.PIPE)
-    #proc = subprocess.Popen(["/usr/local/hadoop/bin/hadoop", "job", "-list"], stdout=subprocess.PIPE)
+    #proc = subprocess.Popen(["/usr/lib/hadoop/bin/hadoop", "job", "-list"], stdout=subprocess.PIPE)
+    proc = subprocess.Popen(["/usr/local/hadoop/bin/hadoop", "job", "-list"], stdout=subprocess.PIPE)
     alljobs, err = proc.communicate()
-    responsestring = {"id": parsed_job["jobid"], "jobId": alljobs[i:i+21]}
+    linecount = alljobs.count('\n')
+    lines = alljobs.splitlines()
+    jobline = lines[linecount-1]
+
+    jobIDIndex = jobline.find("job_")
+    jobID = jobline[jobIDIndex:jobIDIndex+21]
+    responsestring = {"id": parsed_job["jobid"], "jobId": jobID}
     print responsestring
+    rownum = rownum + 1
+
+
     #print alljobs
     ##url = 'http://localhost:8000/job/create/?'
 
@@ -42,9 +53,6 @@ for row in parsed_data:
     ##    url += '%s=%s&' % (key, value)
     ##url = url[:-1]
     ##requests.get(url)
-    i = i+55
-    rownum = rownum + 1
-
 
 
 
@@ -71,4 +79,14 @@ for row in reader:
 	submitjob.newjob(job)
       colnum = colnum + 1
   rownum = rownum + 1
-ifile.close()"""
+ifile.close()
+
+1 jobs currently running
+JobId	State	StartTime	UserName	Priority	SchedulingInfo
+job_201402030754_0223	4	1397500639898	wrahman	NORMAL	NA
+
+2 jobs currently running
+JobId	State	StartTime	UserName	Priority	SchedulingInfo
+job_201402030754_0223	1	1397500639898	wrahman	NORMAL	6 running map tasks using 6 map slots. 0 additional slots reserved. 0 running reduce tasks using 0 reduce slots. 0 additional slots reserved.
+job_201402030754_0224	4	1397500648629	wrahman	NORMAL	NA
+"""
--- APIConsumer/submitjob.py
+++ APIConsumer/submitjob.py
@@ -4,19 +4,19 @@ import subprocess
 
 def newjob(ajob, flagvalue):
 
-  conn = pyhs2.connect(host='136.159.79.112', port = 10000, authMechanism = "PLAIN", user='wrahman', password='suman@ASE13', database='default')
+  #conn = pyhs2.connect(host='136.159.79.112', port = 10000, authMechanism = "PLAIN", user='wrahman', password='suman@ASE13', database='default')
 
-  #conn = pyhs2.connect(host='localhost', port = 10000, authMechanism = "PLAIN", user='hduser', password='suman@ASE13', database='default')
+  conn = pyhs2.connect(host='localhost', port = 10000, authMechanism = "PLAIN", user='hduser', password='suman@ASE13', database='default')
 
   cur = conn.cursor()
-  cur.execute(ajob['createdb'])
-  cur.execute(ajob['workdb'])
-  cur.execute(ajob['droptable1'])
-  cur.execute(ajob['createtable1'])
-  cur.execute(ajob['testcode'])
-  cur.execute(ajob['droptable2'])
-  cur.execute(ajob['createtable2'])
-
+  #cur.execute(ajob['createdb'])
+  #cur.execute(ajob['workdb'])
+  #cur.execute(ajob['droptable1'])
+  #cur.execute(ajob['createtable1'])
+  #cur.execute(ajob['testcode'])
+  #cur.execute(ajob['droptable2'])
+  #cur.execute(ajob['createtable2'])
+  cur.execute(ajob)
           #for i in cur.fetch()
           #    print i
 
